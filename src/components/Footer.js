348f62bba7b2034b395fb00ea359fbaa239c2fec
Robin9203 <robinxd11@gmail.com>
2023-02-15 19:29:20 +0100

    feat: Cambio footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,30 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import "../styles/Footer.css";
+import Nav from "react-bootstrap/Nav";
+
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="socialMedia">
-            <WhatsAppIcon />
-            <EmailIcon />
-            <GitHubIcon />
-            <LinkedInIcon />
-      </div>
+      <Nav className="socialMedia">
+        <Nav.Link href="https://mail.google.com/mail/u/1/#inbox?compose=new" target={"_blank"}>
+          <EmailIcon  />
+        </Nav.Link>
+
+        <Nav.Link href="https://github.com/Robin0311" target={"_blank"}>
+          <GitHubIcon />
+        </Nav.Link>
+
+        <Nav.Link href="https://www.linkedin.com/in/robinson-cerrato/" target={"_blank"}>
+          <LinkedInIcon />
+        </Nav.Link>
+
+        <Nav.Link href="https://api.whatsapp.com/send/?phone=123456789&text&type=phone_number&app_absent=0" target={"_blank"}>
+          <WhatsAppIcon />
+        </Nav.Link>
+      </Nav>
+
       <p> &copy; 2023 PortafolioRobinson.com</p>
     </div>
   );
